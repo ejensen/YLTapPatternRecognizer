917ca5fcff0b14b85f87f0691d93d95f85501186
Eric Jensen <jensen39@gmail.com>
2013-05-16 22:08:52 -0400

    Zelda-fied the demo project

--- Example/Example/STViewController.m
+++ Example/Example/STViewController.m
@@ -9,34 +9,54 @@
 #import "STViewController.h"
 #import "SecretTapGestureRecognizer.h"
 
+#import <AVFoundation/AVFoundation.h>
+
+@interface STViewController ()
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+@end
+
 @implementation STViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+    [self performSelectorInBackground:@selector(prepareChime) withObject:nil];
     [self.view addGestureRecognizer:[[SecretTapGestureRecognizer alloc] initWithTarget:self action:@selector(didSecretTap:)]];
 }
 
-- (void)didSecretTap:(SecretTapGestureRecognizer *)gestureRecognizer
-{
-    NSLog(@"Secret Unlocked!");
-    
-    self.successImageView.alpha = 0;
-    self.successImageView.hidden = NO;
-    [UIView animateWithDuration:0.3 animations:^{
-        self.successImageView.alpha = 1;
-    }];
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     self.successImageView.hidden = YES;
     self.view.backgroundColor = [UIColor blackColor];
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
     self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didSecretTap:(SecretTapGestureRecognizer *)gestureRecognizer {
+    NSLog(@"Secret Unlocked!");
+    
+    [self performSelectorInBackground:@selector(playChime) withObject:nil];
+    
+    self.successImageView.alpha = 0;
+    self.successImageView.hidden = NO;
+    [UIView animateWithDuration:0.5 animations:^{
+        self.successImageView.alpha = 1;
+    }];
+}
+
+- (void)prepareChime {
+    NSURL *soundURL = [[NSBundle mainBundle] URLForResource:@"SecretUnlock" withExtension:@"mp3"];
+    NSError *error;
+    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:&error];
+    if (!error && soundURL) {
+        self.audioPlayer = audioPlayer;
+        audioPlayer.volume = 0.5f;
+        [audioPlayer prepareToPlay];
+    }
+}
+
+- (void)playChime {
+    [self.audioPlayer play];
 }
 
 @end
